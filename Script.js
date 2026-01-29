cf6511b99a8f301bca8c3ac4924812925754d0af
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2026-01-29 21:19:03 +0545

    updated

---
+++ Script.js
@@ -0,0 +1,147 @@
+// console.log("My name is shreesha")
+
+
+// npx create-react-app myportfoilio (Pure React App) 
+// cd myportfoilio
+// npm start - localhost:3000 
+
+
+// npm create vite@latest myportfolio  (react vite app)
+// cd myportfolio
+// npm run dev  -localhost:5173
+
+function getAleart (){
+    window.alert("This is Alert Box");
+}
+
+
+function InsertText(){
+    document.getElementById("jstest").innerHTML = "Thank you for Clicking Button";
+}
+
+// document.write("This is written in js file")
+
+let name = "Shreesha";
+// console.log(name);
+
+
+var myname = "shreesha"; 
+var myname = "kumar";
+
+
+const shreesha = "Hii MY name is Shreesha";
+// console.log(shreesha);
+
+//  =  : Value Assign 
+//  ==  : Compare Value 
+//  ===  : Compare Value and Data Type
+// !=  : Not Equal
+
+ const X = 10;
+const Y = 8;
+
+    // if(X === Y){
+    //     console.log("Both are equal");
+    // }
+//    if (X > Y){
+
+//     if ( X % Y === 2){
+//         console.log("X is greater than Y and remainder is 2");
+//     }
+        
+//     }
+//     else if (X < Y){
+//         console.log("X is less than Y");
+//     }
+//     else {
+//         console.log("Both are not equal");
+//     }
+
+//    const output =   (X < Y) ? "X is greater than Y" : "X is not greater than Y";
+
+
+// switch (true){
+//     case X > Y :
+//         console.log("X is greater than Y");
+//         break ;
+
+//     case X < Y :
+//         console.log("X is less than Y");
+//         break ;
+
+//     default : 
+//     console.log("X is equal to Y");
+// }
+
+// const firstname = "Shreesha";
+// const lastname = "Kumar";
+
+// if (firstname
+//     === "Shreesha" && lastname === "Kumar" &&  ){
+
+//         console.log("Ture both are correct");
+//     }
+
+//     else if ( firstname === "Shreesha" || lastname === "ku"){
+//         console.log("One is correct");
+//     }
+
+//     else if (firstname !="Ravi"){
+//         console.log("Firstname is not Ravi");
+//     }
+
+//     else {
+//         console.log("ALL are incorrect");
+//     }
+
+
+// Js 5 types of Loop 
+// 1. For Loop
+// 2. While Loop
+// 3. Do While Loop
+// 4. For in Loop
+// 5. For of Loop
+
+
+// for (let i = 5; i >= 1; i--){
+//     console.log(i);
+// }
+
+// let  i = 1 ;
+
+// while (i < 10){
+//     console.log(i);
+//     i ++;
+
+// }
+
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++ ;
+// }
+// while(i < 5);
+
+
+// const people = ["Shreesha", "Ravi", "Kumar", "Suresh"];
+
+// for (let person of people){
+//     console.log(person);
+// }
+
+const person = {
+    name : {
+        firstnname:"Shreesha",
+        lastname:"AAtreya"
+    },
+    age : 23,
+    religion :"HIndu"
+}
+
+// for (let key in person ) {
+//     console.log(key + " : " + person[key]);
+// }
+
+
+
+
